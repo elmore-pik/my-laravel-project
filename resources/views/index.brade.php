b76ee0df216bcff9f01fffd47ab23d7adfff4a23
Mai Takada <maimait99@gmail.com>
2025-03-18 02:19:02 +0900

    最初のコミット

---
+++ resources/views/index.brade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        My Laravel App
+    </x-slot:title>
+
+    <h1>Posts</h1>
+        <ul>
+            @forelse ($posts as $index => $post)
+                <li>
+                    <a href="{{ route('posts.show', $index) }}">{{ $post }}</a>
+                </li>
+            @empty
+                <li>No post!</li>
+            @endforelse
+        </ul>
+</x-layout>
